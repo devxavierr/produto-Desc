fd679fed625aa6d00df64f67c9d8459dfb86e343
devxavierr <devxavierr@gmail.com>
2023-10-29 10:00:34 -0300

    init

---
+++ src/main/java/br/com/siteware/sitewareProduto/SitewareProdutoApplication.java
@@ -0,0 +1,13 @@
+package br.com.siteware.sitewareProduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitewareProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitewareProdutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/siteware/sitewareProduto/SitewareProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.siteware.sitewareProduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitewareProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
