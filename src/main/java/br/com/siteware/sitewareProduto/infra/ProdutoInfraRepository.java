1ee08c1c69cf4b852a3cda5530c72842f40501f1
devxavierr <devxavierr@gmail.com>
2023-11-02 18:47:03 -0300

    implementa produto repository - comunicacao ok com o banco

--- src/main/java/br/com/siteware/sitewareProduto/infra/ProdutoInfraRepository.java
+++ src/main/java/br/com/siteware/sitewareProduto/infra/ProdutoInfraRepository.java
@@ -2,15 +2,20 @@ package br.com.siteware.sitewareProduto.infra;
 
 import br.com.siteware.sitewareProduto.application.repository.ProdutoRepository;
 import br.com.siteware.sitewareProduto.domain.Produto;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Log4j2
+@RequiredArgsConstructor
 public class ProdutoInfraRepository implements ProdutoRepository {
+    private final ProdutoSpringDataJPARepository produtoSpringDataJPARepository;
+
     @Override
     public Produto salva(Produto produto) {
         log.info("[inicia] ProdutoInfraRepository - salva");
+        produtoSpringDataJPARepository.save(produto);
         log.info("[finaliza] ProdutoInfraRepository - salva");
         return produto;
     }
