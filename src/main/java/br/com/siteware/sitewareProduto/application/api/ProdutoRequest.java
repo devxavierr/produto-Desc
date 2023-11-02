5ecb8bf2b692ee70d6e7664ebac4b7b36814a3da
devxavierr <devxavierr@gmail.com>
2023-11-02 17:10:41 -0300

    implementa requeste e response

--- src/main/java/br/com/siteware/sitewareProduto/application/api/ProdutoRequest.java
+++ src/main/java/br/com/siteware/sitewareProduto/application/api/ProdutoRequest.java
@@ -1,4 +1,16 @@
 package br.com.siteware.sitewareProduto.application.api;
 
+import br.com.siteware.sitewareProduto.domain.StatusPromocao;
+import lombok.Value;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+import java.util.UUID;
+@Value
 public class ProdutoRequest {
+    @NotBlank
+    private String nome;
+    @NotBlank
+    private double preco;
+    private StatusPromocao statusPromocao;
 }
--- src/main/java/br/com/siteware/sitewareProduto/application/api/ProdutoResponse.java
+++ src/main/java/br/com/siteware/sitewareProduto/application/api/ProdutoResponse.java
@@ -1,4 +1,9 @@
 package br.com.siteware.sitewareProduto.application.api;
 
+import lombok.Value;
+
+import java.util.UUID;
+@Value
 public class ProdutoResponse {
+    private UUID idProduto;
 }
