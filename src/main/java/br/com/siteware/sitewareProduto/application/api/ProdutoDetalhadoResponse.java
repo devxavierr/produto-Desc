10f6457b3d6ffec784e871d091557ed0758ac32e
devxavierr <devxavierr@gmail.com>
2023-11-03 21:09:26 -0300

    finaliza metodo busca produto por id

--- src/main/java/br/com/siteware/sitewareProduto/application/api/ProdutoDetalhadoResponse.java
+++ src/main/java/br/com/siteware/sitewareProduto/application/api/ProdutoDetalhadoResponse.java
@@ -1,13 +1,27 @@
 package br.com.siteware.sitewareProduto.application.api;
 
+import br.com.siteware.sitewareProduto.domain.Produto;
 import br.com.siteware.sitewareProduto.domain.StatusPromocao;
+import lombok.Value;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
+@Value
 public class ProdutoDetalhadoResponse {
+    private UUID idProduto;
     private String nome;
     private String preco;
     private StatusPromocao statusPromocao;
 
     private LocalDateTime dataHoraDoCadastro;
+
+    public ProdutoDetalhadoResponse(Produto produto) {
+        this.idProduto = produto.getIdProduto();
+        this.nome = produto.getNome();
+        this.preco = produto.getPreco();
+        this.statusPromocao = produto.getStatusPromocao();
+        this.dataHoraDoCadastro = produto.getDataHoraDoCadastro();
+    }
+
 }
--- src/main/java/br/com/siteware/sitewareProduto/infra/ProdutoInfraRepository.java
+++ src/main/java/br/com/siteware/sitewareProduto/infra/ProdutoInfraRepository.java
@@ -2,11 +2,14 @@ package br.com.siteware.sitewareProduto.infra;
 
 import br.com.siteware.sitewareProduto.application.repository.ProdutoRepository;
 import br.com.siteware.sitewareProduto.domain.Produto;
+import br.com.siteware.sitewareProduto.handler.APIException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 @Log4j2
@@ -28,5 +31,14 @@ public class ProdutoInfraRepository implements ProdutoRepository {
         List<Produto> todosProdutoss = produtoSpringDataJPARepository.findAll();
         log.info("[finaliza] ProdutoInfraRepository - buscaTodosProdutos");
         return todosProdutoss;
+    }
+
+    @Override
+    public Produto buscaProdutoAtravesId(UUID idProduto) {
+        log.info("[inicia] ProdutoInfraRepository - buscaProdutoAtravesId");
+        Produto produto = produtoSpringDataJPARepository.findById(idProduto)
+                .orElseThrow(()-> APIException.build(HttpStatus.NOT_FOUND, "Produto não encontrado!"));
+        log.info("[finaliza] ProdutoInfraRepository - buscaProdutoAtravesId");
+        return produto;
     }
 }
